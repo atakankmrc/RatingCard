40a1523a9310584645ad56745878a89d9c21302a
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-02-18 11:19:32 +0300

    Initial Commit

---
+++ RatingCard/RatingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingCardApp.swift
+//  RatingCard
+//
+//  Created by Atakan Kömürcü on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct RatingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
